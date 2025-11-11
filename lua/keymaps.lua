f16302c37b8429a748c21156042ba1e139a0a839
dmclink <135415819+dmclink@users.noreply.github.com>
2025-11-11 22:07:06 +0700

    remove old remap interfering with auto complete

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -40,5 +40,3 @@ vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
 vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'make file e[X]ecutable' })
-
-vim.keymap.set('i', '<C-a>', '<C-y>', { noremap = true, silent = true })
