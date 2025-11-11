16670ec307a69c97ff6533490eccdf60b63ae770
dmclink <135415819+dmclink@users.noreply.github.com>
2025-11-11 22:29:24 +0700

    add custom html snippet

--- init.lua
+++ init.lua
@@ -814,7 +814,9 @@ require('lazy').setup({
           {
             'rafamadriz/friendly-snippets',
             config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load {
+                paths = { vim.fn.stdpath 'config' .. '/snippets' },
+              }
             end,
           },
         },
