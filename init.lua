f37aaa403350f5e8c3633a445c22ff7d9744064e
dmclink <135415819+dmclink@users.noreply.github.com>
2025-11-11 22:06:00 +0700

    remap autocompletion accepts

--- init.lua
+++ init.lua
@@ -847,8 +847,8 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
-
+        preset = 'none',
+        ['<C-a'] = { 'select_and_accept', 'fallback' },
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
