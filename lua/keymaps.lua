b11693dd0081e599043b9eb4cd8a9a9c5d1e77a6
dmclink <135415819+dmclink@users.noreply.github.com>
2025-11-12 17:22:55 +0700

    fix keymap issue with moving lines in visualmode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 
 vim.keymap.set('n', '<leader>u', ':UndotreeShow<CR>')
 
-vim.keymap.set('v', 'J', ':m ">+1<CR>gv=gv')
-vim.keymap.set('v', 'K', ':m "<-2<CR>gv=gv')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('n', 'J', 'mzJ`z')
 
